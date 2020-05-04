80ddeafd0846a5d5267d6c678b154b128072b958
vksharma <vikram.sharma1024@gmail.com>
2020-05-04 14:06:52 -0400

    insert first round of images for carousels

--- ababu-client/src/components/inserts/InstaFeed.js
+++ ababu-client/src/components/inserts/InstaFeed.js
@@ -1,13 +1,17 @@
 import React, {Component} from 'react'
 import 'react-responsive-carousel/lib/styles/carousel.min.css'
 
+import img1 from '../images/IMG_0836_c.jpeg'
+import img2 from '../images/IMG_3310_c.jpeg'
+import img3 from '../images/IMG_5599_c.jpg'
+
 class InstaFeed extends Component {
     render(){
         return(
             <div className="insta-feed">
-                <iframe className="insta-post" src="https://www.instagram.com/p/B53IXFbj4Qm/embed" frameBorder="0" scrolling="no" allowtransparency="true" title="highlight-1"/>
-                <iframe className="insta-post" src="https://www.instagram.com/p/B53IXFbj4Qm/embed" frameBorder="0" scrolling="no" allowtransparency="true" title="highlight-2"/>
-                <iframe className="insta-post" src="https://www.instagram.com/p/B53IXFbj4Qm/embed" frameBorder="0" scrolling="no" allowtransparency="true" title="highlight-3"/>
+                <img src={img1} alt="Abiy, his client Christian, and his client/co-handler Miguel" className="insta-post"/>
+                <img src={img2} alt="Abiy and 3 of his clients after the MD state championships" className="insta-post"/>
+                <img src={img3} alt="Abiy and friends at his home gym, Chiseled Life" className="insta-post"/>
             </div>
         )
     }
--- ababu-client/src/components/inserts/MobileInstaCarousel.js
+++ ababu-client/src/components/inserts/MobileInstaCarousel.js
@@ -2,6 +2,10 @@ import React, {Component} from 'react'
 import {Carousel} from 'react-responsive-carousel'
 import 'react-responsive-carousel/lib/styles/carousel.min.css'
 
+import img1 from '../images/IMG_0836_c.jpeg'
+import img2 from '../images/IMG_3310_c.jpeg'
+import img3 from '../images/IMG_5599_c.jpg'
+import img4 from '../images/IMG_5588_c.jpg'
 
 class MobileInstaCarousel extends Component{
     render(){
@@ -11,15 +15,20 @@ class MobileInstaCarousel extends Component{
                     Carousel infiniteLoop showThumbs={false} showArrows={false}
                     showIndicators={false}
                     swipeable={true}
+                    useKeyboardArrows
+                    autoPlay
                 >
                     <div>
-                        <iframe className="insta-post" src="https://www.instagram.com/p/B53IXFbj4Qm/embed" frameBorder="0" scrolling="no" allowtransparency="true" title="highlight-1"/>
+                        <img src={img1} alt="Abiy, his client Christian, and his client/co-handler Miguel"/>
                     </div>
                     <div>
-                        <iframe className="insta-post" src="https://www.instagram.com/p/B53IXFbj4Qm/embed" frameBorder="0" scrolling="no" allowtransparency="true" title="highlight-1"/>
+                        <img src={img2} alt="Abiy and 3 of his clients after the MD state championships"/>
                     </div>
                     <div>
-                        <iframe className="insta-post" src="https://www.instagram.com/p/B53IXFbj4Qm/embed" frameBorder="0" scrolling="no" allowtransparency="true" title="highlight-1"/>
+                        <img src={img3} alt="Abiy and friends at his home gym, Chiseled Life"/>
+                    </div>
+                    <div>
+                        <img src={img4} alt="Abiy and Sammy congradulating his client James on a meet well done."/>
                     </div>
                 </Carousel>
             </div>
